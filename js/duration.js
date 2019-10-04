982a7e167f904a5ecbfc74dd755a40f3b964b915
R Steiner <r.steiner@uconn.edu>
2019-10-03 21:15:51 -0400

    Fix type conversion.

--- js/duration.js
+++ js/duration.js
@@ -32,10 +32,14 @@ function sameEra(startEra, endEra) {
 }
 
 function duration() {
-  let startEra = $("#start-era-input").value;
-  let startYear = $("#start-year-input").value;
-  let endEra = $("#end-era-input").value;
-  let endYear = $("#end-year-input").value;
+  let startEra = parseInt($("#start-era-input").val());
+  let startYear = parseInt($("#start-year-input").val());
+  let endEra = parseInt($("#end-era-input").val());
+  let endYear = parseInt($("#end-year-input").val());
+  console.log("start era: " + typeof startEra);
+  console.log("start year: " + typeof startYear);
+  console.log("end era: " + typeof endEra);
+  console.log("end year: " + typeof endYear);
   if (!(withinRange(startEra, startYear, eraDurs) && withinRange(endEra, endYear, eraDurs))) {
     return "Bad range";
   }
@@ -47,22 +51,31 @@ function duration() {
 
 function calculateDuration(startEra, startYear, endEra, endYear, eras) {
   if (sameEra(startEra, endEra)) {
+    console.log("same era");
     return endYear - startYear;
   }
   if (endEra - startEra === 1) {
+    console.log("consecutive eras");
     return (eras[startEra] - startYear) + endYear;
   }
+  console.log("non-consecutive eras");
   return _.reduce(eras, (accum, eraDur, era) => {
-    if (era === startEra) {
+    console.log(era);
+    let eraInt = parseInt(era);
+    if (eraInt === startEra) {
+      console.log("...is the start era");
       // If it's the start era, add what's left of the era.
       return accum + (eraDur - startYear);
-    } else if (era === endEra) {
+    } else if (eraInt === endEra) {
+      console.log("...is the end era");
       // If it's the end era, add the year.
       return accum + endYear;
-    } else if ((era > startEra) && (era < endEra)) {
+    } else if ((eraInt > startEra) && (eraInt < endEra)) {
+      console.log("...is an intermediate era");
       // If it's an intermediate era, add its duration.
       return accum + eraDur;
     } else {
+      console.log("...is an irrelevant era");
       // If it's not a relevant era, pass on the current accumulator.
       return accum;
     }
@@ -72,6 +85,7 @@ function calculateDuration(startEra, startYear, endEra, endYear, eras) {
 function setDuration() {
   // Run duration(), then put result in element.
   $("#result").text(duration);
+  console.log("");
 }
 
 // Have a nice visual break.
