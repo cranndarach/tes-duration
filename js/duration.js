b37324421a1f33815a4e1b0bf3a375c70c35d099
R Steiner <r.steiner@uconn.edu>
2019-10-03 20:47:59 -0400

    Finish main logic.

--- js/duration.js
+++ js/duration.js
@@ -32,16 +32,41 @@ function sameEra(startEra, endEra) {
 }
 
 function duration() {
-  let startEra = $("#start-era-input").value();
-  let startYear = $("#start-year-input").value();
-  let endEra = $("#end-era-input").value();
-  let endYear = $("#end-year-input").value();
+  let startEra = $("#start-era-input").value;
+  let startYear = $("#start-year-input").value;
+  let endEra = $("#end-era-input").value;
+  let endYear = $("#end-year-input").value;
   if (!(withinRange(startEra, startYear, eraDurs) && withinRange(endEra, endYear, eraDurs))) {
-    return "Bad range.";
+    return "Bad range";
   }
   if (!sequential(startEra, startYear, endEra, endYear)) {
-    return "Dates not sequential.";
+    return "Dates not sequential";
   }
+  return calculateDuration(startEra, startYear, endEra, endYear, eraDurs).toLocaleString() + " years";
+}
+
+function calculateDuration(startEra, startYear, endEra, endYear, eras) {
+  if (sameEra(startEra, endEra)) {
+    return endYear - startYear;
+  }
+  if (endEra - startEra === 1) {
+    return (eras[startEra] - startYear) + endYear;
+  }
+  return _.reduce(eras, (accum, eraDur, era) => {
+    if (era === startEra) {
+      // If it's the start era, add what's left of the era.
+      return accum + (eraDur - startYear);
+    } else if (era === endEra) {
+      // If it's the end era, add the year.
+      return accum + endYear;
+    } else if ((era > startEra) && (era < endEra)) {
+      // If it's an intermediate era, add its duration.
+      return accum + eraDur;
+    } else {
+      // If it's not a relevant era, pass on the current accumulator.
+      return accum;
+    }
+  }, 0);
 }
 
 function setDuration() {
@@ -49,6 +74,7 @@ function setDuration() {
   $("#result").text(duration);
 }
 
+// Have a nice visual break.
 $(document).ready(() => {
   $(".form-control").change(setDuration);
 });
